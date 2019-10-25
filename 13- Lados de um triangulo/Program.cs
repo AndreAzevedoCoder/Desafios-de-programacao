37d998ed9a0ae240ddc31a1ba4b2bfd7ec23d44e
André Azevedo <andreazevedocoder@gmail.com>
2019-10-25 11:27:02 -0300

    Quase la

--- 13- Lados de um triangulo/Program.cs
+++ 13- Lados de um triangulo/Program.cs
@@ -6,7 +6,18 @@ namespace _13__Lados_de_um_triangulo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Digite o primeiro numero: ");
+            double num1 = Double.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o segundo numero: ");
+            double num2 = Double.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o terceiro numero: ");
+            double num3 = Double.Parse(Console.ReadLine());
+
+            if(num3 >= 0 && num2 >= 0 && num1 >= 0){
+                System.Console.WriteLine("Podem ser lados de um triangulo");
+            }else{
+                System.Console.WriteLine("Estes não podem ser lados de um triangulo");
+            }
         }
     }
 }
